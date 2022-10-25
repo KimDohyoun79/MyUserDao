8509f19543450171854258cd153cd380665c5b9b
dokim <dogdo5726@gmail.com>
2022-10-25 15:37:06 +0900

    deleteAll, getCount tester2

--- src/test/java/com/example/dao/UserDaoTest.java
+++ src/test/java/com/example/dao/UserDaoTest.java
@@ -2,6 +2,7 @@ package com.example.dao;
 
 import com.example.domain.User;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -21,10 +22,17 @@ class UserDaoTest {
     @Autowired
     ApplicationContext context;
 
+    UserDao userDao;
+    @BeforeEach
+    void setUp() {
+        this.userDao = context.getBean("awsUserDao", UserDao.class);
+    }
+
     //4. addAndGet 테스트 만들기
     @Test
     @DisplayName("addAndGet")
     void addAndGet() throws SQLException {
+
         UserDao userDao = context.getBean("awsUserDao", UserDao.class);
         //deleteAll
         userDao.deleteAll();
@@ -39,8 +47,28 @@ class UserDaoTest {
         User getUser = userDao.findById(id);
         Assertions.assertEquals("두현", getUser.getName());
 
-        //getCount
-        assertEquals(1, userDao.getCount());
+    }
+
+    @Test
+    void count() throws SQLException {
+        User user1 = new User("1","11","111");
+        User user2 = new User("2","22","222");
+        User user3 = new User("3","33","333");
+
+        userDao.deleteAll();
+        assertEquals(0, userDao.getCount());
+
+        userDao.add(user1);
+        userDao.add(user2);
+        userDao.add(user3);
+        assertEquals(3, userDao.getCount());
+    }
+
+    @Test
+    void findByid(){
+        //findByid
+        User getUser = userDao.findById("2");
+        Assertions.assertEquals("22", getUser.getName());
 
     }
 }
